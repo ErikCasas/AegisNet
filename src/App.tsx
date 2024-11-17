0d05a5c3c65cd81d05af021e2e48857ec6a89c7a
Erik <santicasas667@gmail.com>
2024-11-17 12:07:22 -0500

    base project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from "./components/NavBar";
+import { Router } from "./Router";
+import "./App.css";
+
+export function App() {
+  return (
+    <>
+      <NavBar />
+      <Router />
+    </>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Suspense fallback={<h1>Holaa</h1>}>
+        <App />
+      </Suspense>
+    </BrowserRouter>
+  </StrictMode>
+);
